0db0995baa54859cf62647aefe6360d4d0bedee6
Gregory Vincent <gregvjrr@gmail.com>
2022-11-14 15:40:09 -0500

    Test commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+/**
+ * @author Gregory Vincent
+ * Group of 4 authors
+ * Alex Urmaza, Bryan Gomez, Greg Vincent, AJ Discorsi
+ * 11/14/22
+ */
+
+function App(){
+    return(
+        <h1 className = "first-h1">Hello World.</h1>
+    );
+}
+
+export default App;
