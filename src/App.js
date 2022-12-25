4d354b9f2c9b51fec36d5e877518e7e3b097ddc1
Greg Vincent <gavincent@albany.edu>
2022-12-25 14:43:43 -0500

    Finished Popup Version 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import Popup from "./components/Popup";
-
-/**
- * all rendered components 'rendered' as part of their name
- */
 function App() {
-  const [visiblePopup, setVisiblePopup] = useState(true);
-
   return (
-    <div>
-      {/**the trigger value determines if popup is rendered, but this value is set elsewhere */}
-      <Popup className="rendered-popup" trigger={visiblePopup}>
-        {/**Because the h1 is a child of the popup component, all css for it is handled in Popup.jsx */}
-        <h1>Join or Chat</h1>
-      </Popup>
-      <h1>Placeholder text</h1>
+    <div className="app-container">
+      <Popup />
     </div>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,18 +1,91 @@
 import React from "react";
+import { useState, useRef } from "react";
 import "./Popup.css";
 /**
  * @author Greg Vincent
  * @date 12/15/22
- *On page load, this popup will have a form allowing the 
-  user to sign into/up for the messaging service so they can use it.
+ * First version of this Popup
+ * In future versions with more experience, less state will be used
  */
 function Popup(props) {
-  //conditionally rendering based on trigger value
-  return props.trigger ? (
+  //sets popup visibility
+  const [visiblePopup, setVisiblePopup] = useState(true);
+
+  //keep a reference to submitted values
+  const nameRef = useRef();
+  const passwordRef = useRef();
+
+  //used for validating those two form values - if they're empty, display a message
+  const [nameIsValid, setIsNameValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  //Only display message after form is submitted
+  const [wasTouched, setWasTouched] = useState(false);
+
+  //button animation state
+  const [hovering, setHovering] = useState(false);
+
+  //username and password checking on submit
+  function handleSubmit(event) {
+    event.preventDefault();
+    setWasTouched(true);
+    //using String casting for the length property
+    const submittedName = String(nameRef.current.value).trim();
+    const submittedPassword = String(passwordRef.current.value).trim();
+    submittedName.length === 0 ? setIsNameValid(false) : setIsNameValid(true);
+    submittedPassword.length === 0
+      ? setPasswordIsValid(false)
+      : setPasswordIsValid(true);
+    nameIsValid && passwordIsValid
+      ? setVisiblePopup(false)
+      : setVisiblePopup(true);
+  }
+
+  //functions for mouseOn/mouseOff hover animation
+  const mouseEnter = () => {
+    setHovering(true);
+  };
+
+  const mouseExit = () => {
+    setHovering(false);
+  };
+
+  return visiblePopup ? (
     <div className="Popup">
       <div className="Popup-inner">
-        <button className="exit-btn">X</button>
         {props.children}
+        <h1>
+          <em>Join the Conversation</em>
+        </h1>
+        <br /> <br />
+        <input type="text" placeholder="       username" ref={nameRef} />
+        {!nameIsValid && wasTouched && (
+          <p className="error-message" id="error-message-name">
+            Username can't be empty.
+          </p>
+        )}
+        <br /> <br />
+        <input
+          type="text"
+          id="password-input"
+          placeholder="       password"
+          ref={passwordRef}
+        />
+        {!passwordIsValid && wasTouched && (
+          <p className="error-message" id="error-message-password">
+            Password can't be empty.
+          </p>
+        )}
+        <br /> <br />
+        <button
+          type="Submit"
+          onClick={handleSubmit}
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseExit}
+          className={hovering ? "hovering" : "not-hovering"}
+        >
+          Submit
+        </button>
       </div>
     </div>
   ) : (
