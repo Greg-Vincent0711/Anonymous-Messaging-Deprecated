85f686085561ed75b51e4108a2449ba11447a560
Greg Vincent <gavincent@albany.edu>
2022-12-31 16:49:17 -0500

    Added more functionality to popup.

--- src/WhyPage/WhyPage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./Why.css";
-function WhyPage() {}
-
-export default WhyPage;
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,37 +5,47 @@ import "./Popup.css";
  * @author Greg Vincent
  * @date 12/15/22
  * First version of this Popup
- * In future versions with more experience, less state will be used
+ * TODO - recreate logic without using state so much
+ * TODO - Finishing adding onBlur animation
+ * TODO - add regex to form validation. Can't have special characters.
+ * const validExpr = /^[A-Za-z]+$/; - regex for only A-Z and a-z
+ * all todos will be in a future update
  */
 function Popup(props) {
   //sets popup visibility
   const [visiblePopup, setVisiblePopup] = useState(true);
 
-  //keep a reference to submitted values
+  //ref for submission validation
   const nameRef = useRef();
   const passwordRef = useRef();
 
-  //used for validating those two form values - if they're empty, display a message
+  //if either is empty and the form is touched display a value
   const [nameIsValid, setIsNameValid] = useState(false);
-  const [passwordIsValid, setPasswordIsValid] = useState(false);
+  const [passwordIsValid, setIsPasswordValid] = useState(false);
+  const [wasNameTouched, setWasNameTouched] = useState(false);
+  const [wasPasswordTouched, setWasPasswordTouched] = useState(false);
 
-  //Only display message after form is submitted
-  const [wasTouched, setWasTouched] = useState(false);
+  //needed for instant validation for a user typing
+  const [enteredName, setEneteredName] = useState("");
+  const [enteredPassword, setEneteredPassword] = useState("");
 
   //button animation state
   const [hovering, setHovering] = useState(false);
 
-  //username and password checking on submit
+  /**
+   * Validates with ref on submit
+   */
   function handleSubmit(event) {
     event.preventDefault();
-    setWasTouched(true);
-    //using String casting for the length property
+    setWasNameTouched(true);
+    setWasPasswordTouched(true);
+
     const submittedName = String(nameRef.current.value).trim();
     const submittedPassword = String(passwordRef.current.value).trim();
-    submittedName.length === 0 ? setIsNameValid(false) : setIsNameValid(true);
-    submittedPassword.length === 0
-      ? setPasswordIsValid(false)
-      : setPasswordIsValid(true);
+    validate(submittedName) ? setIsNameValid(false) : setIsNameValid(true);
+    validate(submittedPassword)
+      ? setIsPasswordValid(false)
+      : setIsPasswordValid(true);
     nameIsValid && passwordIsValid
       ? setVisiblePopup(false)
       : setVisiblePopup(true);
@@ -50,6 +60,60 @@ function Popup(props) {
     setHovering(false);
   };
 
+  //quick development function
+  // function tempSubmit() {
+  //   setVisiblePopup(true);
+  // }
+
+  /**
+   * This function will grow with regex
+   */
+  const validate = (arg) => {
+    if (arg.trim() === "") {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setWasNameTouched(true);
+    setEneteredName(event.target.value);
+    if (validate(enteredName)) {
+      setIsNameValid(true);
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    setWasPasswordTouched(true);
+    setEneteredPassword(event.target.value);
+    if (validate(enteredPassword)) {
+      setIsPasswordValid(true);
+    }
+  };
+
+  /**
+   * onChange and onBlur functions
+   * Both need to work with state
+   * onSubmit needs ref
+   */
+
+  const handleNameBlur = () => {
+    setWasNameTouched(true);
+    if (!validate(enteredName)) {
+      setIsNameValid(false);
+      return;
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    setWasPasswordTouched(true);
+    if (!validate(enteredPassword)) {
+      setIsPasswordValid(false);
+      return;
+    }
+  };
+
   return visiblePopup ? (
     <div className="Popup">
       <div className="Popup-inner">
@@ -58,8 +122,15 @@ function Popup(props) {
           <em>Join the Conversation</em>
         </h1>
         <br /> <br />
-        <input type="text" placeholder="       username" ref={nameRef} />
-        {!nameIsValid && wasTouched && (
+        <input
+          type="text"
+          placeholder="       username"
+          ref={nameRef}
+          onBlur={handleNameBlur}
+          onChange={handleNameChange}
+          value={enteredName}
+        />
+        {!nameIsValid && wasNameTouched && (
           <p className="error-message" id="error-message-name">
             Username can't be empty.
           </p>
@@ -70,8 +141,11 @@ function Popup(props) {
           id="password-input"
           placeholder="       password"
           ref={passwordRef}
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={enteredPassword}
         />
-        {!passwordIsValid && wasTouched && (
+        {!passwordIsValid && wasPasswordTouched && (
           <p className="error-message" id="error-message-password">
             Password can't be empty.
           </p>
